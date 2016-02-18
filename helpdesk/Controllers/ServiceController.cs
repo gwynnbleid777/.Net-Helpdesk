675eb5c33e56d88878f8e12c15d549d33efda6ba
gwynnbleid777 <eric777@i.ua>
2016-02-18 10:38:39 +0200

    Add project

---
+++ helpdesk/Controllers/ServiceController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using helpdesk.Models;
+
+namespace helpdesk.Controllers
+{
+    [Authorize(Roles = "Администратор")]
+    public class ServiceController :  Controller
+    {
+        HelpDeskContext db = new HelpDeskContext();
+        [HttpGet]
+        public ActionResult Departments()
+        {
+            ViewBag.Departments = db.Departments;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Departments(Department depo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Departments.Add(depo);
+                db.SaveChanges();
+            }
+            ViewBag.Departments = db.Departments;
+            return View(depo);
+        }
+
+        public ActionResult DeleteDepartment(int id)
+        {
+            Department depo = db.Departments.Find(id);
+            db.Departments.Remove(depo);
+            db.SaveChanges();
+            return RedirectToAction("Departments");
+        }
+
+        [HttpGet]
+        public ActionResult Activ()
+        {
+            ViewBag.Activs = db.Activs.Include(s => s.Department);
+            ViewBag.Departments = new SelectList(db.Departments, "Id", "Name");
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Activ(Activ activ)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Activs.Add(activ);
+                db.SaveChanges();
+            }
+            ViewBag.Activs = db.Activs.Include(s => s.Department);
+            ViewBag.Departments = new SelectList(db.Departments, "Id", "Name");
+            return View(activ);
+        }
+
+        public ActionResult DeleteActiv(int id)
+        {
+            Activ activ = db.Activs.Find(id);
+            db.Activs.Remove(activ);
+            db.SaveChanges();
+            return RedirectToAction("Activ");
+        }
+
+
+        [HttpGet]
+        public ActionResult Categories()
+        {
+            ViewBag.Categories = db.Categories;
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Categories(Category cat)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(cat);
+                db.SaveChanges();
+            }
+            ViewBag.Categories = db.Categories;
+            return View(cat);
+        }
+
+        public ActionResult DeleteCategory(int id)
+        {
+            Category cat = db.Categories.Find(id);
+            db.Categories.Remove(cat);
+            db.SaveChanges();
+            return RedirectToAction("Categories");
+        }
+    }
+}
---
+++ helpdesk/Models/Request.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace helpdesk.Models
+{
+    public class Request
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Название заявки")]
+        [MaxLength(50, ErrorMessage = "Превышена максимальная длина записи")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Описание")]
+        [MaxLength(200, ErrorMessage = "Превышена максимальная длина записи")]
+        public string Description { get; set; }
+
+        [Display(Name = "Комментарий")]
+        [MaxLength(200, ErrorMessage = "Превышена максимальная длина записи")]
+        public string Comment { get; set; }
+
+        [Display(Name = "Статус")]
+        public int Status { get; set; }
+
+        [Display(Name = "Приоритет")]
+        public int Priority { get; set; }
+
+        [Display(Name = "Кабинет")]
+        public int? ActivId { get; set; }
+        public Activ Activ { get; set; }
+
+        [Display(Name = "Файл с ошибкой")]
+        public string File { get; set; }
+
+        // Внешний ключ Категория
+        [Display(Name = "Категория")]
+        public int? CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        // Внешний ключ
+        // ID Пользователя - обычное свойство
+        public int? UserId { get; set; }
+        // Отдел пользователя - Навигационное свойство
+        public User User { get; set; }
+
+        // Внешний ключ
+        // ID Пользователя - обычное свойство
+        public int? ExecutorId { get; set; }
+        // Отдел пользователя - Навигационное свойство
+        public User Executor { get; set; }
+
+        // Внешний ключ
+        // ID жизненного цикла заявки - обычное свойство
+        public int LifecycleId { get; set; }
+        // Ссылка на жизненный цикл заявки - Навигационное свойство
+        public Lifecycle Lifecycle { get; set; }
+    }
+
+    public class Lifecycle
+    {
+        // ID 
+        public int Id { get; set; }
+        // Дата открытия
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy H:mm:ss}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime? Opened { get; set; }
+        // Дата распределения
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy H:mm:ss}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime? Distributed { get; set; }
+        // Дата обработки
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy H:mm:ss}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime? Proccesing { get; set; }
+        // Дата проверки
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy H:mm:ss}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime? Checking { get; set; }
+        // Дата закрытия
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy H:mm:ss}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime? Closed { get; set; }
+    }
+
+    // Перечисление для статуса заявки
+    public enum RequestStatus
+    {
+        Open = 1,
+        Distributed = 2,
+        Proccesing = 3,
+        Checking = 4,
+        Closed = 5
+    }
+    // Перечисление для приоритета заявки
+    public enum RequestPriority
+    {
+        Low = 1,
+        Medium = 2,
+        High = 3,
+        Critical = 4
+    }
+}
